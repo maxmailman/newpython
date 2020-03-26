fd0818150afd8a7fce8364d878add80b2c8f3f93
Max <maxkron@hotmail.com>
2020-03-26 22:28:02 +0700

    55 страница

--- 01.Итерация списка/Словари словарей.py
+++ 01.Итерация списка/Словари словарей.py
@@ -1,5 +1,5 @@
 kab315 = dict(brand='keyocera', model=2040, number=4000)
-kab625 = dict(brand='samsung', model=42000, number=54000)
+kab625 = dict(brand='samsung', model=4200, number=54000)
 kab626 = dict(brand='xerox', model=1030, number=10000)
 kab627 = dict(brand='keyocera', model=2540, number=15000)
 
@@ -30,6 +30,23 @@ print(x)
 
 x = [rec['brand'] for rec in db.values()]
 print(x)
+
+db['kab314'] = dict(brand='hp', model=4515, number=85669)
+db['kab626'] = dict(brand='xerox', model=1030, number=1000)
+db['kab627'] = dict(brand='keyocera', model=2540, number=15000)
+pprint.pprint(db)
+
+print(list(db.keys()))
+print(len(db))
+
+print([rec['model'] for rec in db.values() if rec['brand'] == 'keyocera'])  # SQL подобный запрос
+
+print([rec['model'] for rec in db.values() if 4000 < rec['number'] < 80000]) # SQL подобный запрос
+
+
+
+
+
 
 
 
